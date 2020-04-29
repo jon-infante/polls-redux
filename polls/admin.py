d33efd2651237a66acd9fe2af64393d043005d7d
inv0w <jonjoninfante@gmail.com>
2020-04-28 17:29:03 -0700

    Part 5

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Question
+from . import models
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(models.Question)
+admin.site.register(models.Choice)
+
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
 from django.template import loader
-from .models import Question, Choice
+from django.utils import timezone
+
 from django.urls import reverse
 from django.views import generic
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .models import Choice, Question
 
 # Old functions
 
@@ -26,8 +30,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
+        return Question.objects.filter(
+                pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
@@ -55,5 +60,14 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class DetailView(generic.DetailView):
+    ...
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
